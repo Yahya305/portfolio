ca3f3b8aa1f699828229ef30732b78b42edd1ccf
Yahya305 <saimyahya47@gmail.com>
2025-01-14 02:47:29 +0500

    feat: Intro responsiveness

--- src/app/home/components/Intro.tsx
+++ src/app/home/components/Intro.tsx
@@ -1,33 +1,68 @@
 import ConcentricCircles from "@components/ui/ConcentricCircles";
 import Wrapper from "@components/Wrapper";
 import Image from "next/image";
+import Link from "next/link";
 import React, { Fragment } from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FiDownload } from "react-icons/fi";
+import { cn } from "@lib/utils";
 
-function Section1() {
+function Section1({ className }: { className?: string }) {
     return (
-        <div>
-            <h2>
-                Hello! I'm <div className="text-[90px]">Yahya Salman</div>
+        <div className={cn("", className)}>
+            <h2 className="lg:text-[50px] xl:text-[70px]">
+                Hello! I'm{" "}
+                <div className="lg:text-[70] xl:text-[90px]">Yahya Salman</div>
             </h2>
-            <div className="text-lg text-secondary-foreground">
+            <div className="text-lg text-secondary-foreground lg:w-[270px] xl:w-[439px]">
                 UI/UX Designer specializing in Shopify & Webflow.
             </div>
-            <button className="p-8 border rounded-[50px] hover:bg-border my-10">Get Resume</button>
+            <button className="flex items-center gap-2 relative p-8 border rounded-[50px] bg-background hover:bg-border my-10 z-10">
+                Get Resume <FiDownload size={20} />
+            </button>
         </div>
     );
 }
-function Section2() {
+function Section2({ className }: { className?: string }) {
     return (
-        <div className="relative flex">
+        <div
+            className={cn(
+                "relative flex w-[350px] sm:w-[400px] lg:w-[500px] sm:mx-auto xs:ml-[20px]",
+                className
+            )}
+        >
             <Image
                 src="/Yahya_Salman_Img2.png"
                 width={500}
                 height={200}
                 alt="Yahya Img"
             />
-            <div className="absolute bottom-1 -right-36 h-full w-[400px] overflow-hidden">
+            <div className="absolute bottom-1 xs:-right-44 sm:-right-36 h-full w-[400px] overflow-hidden">
                 <div className="relative w-[400px] ">
-                    <ConcentricCircles className="left-[90px] -z-10 top-[350px]" />
+                    <Link
+                        href="/"
+                        className="border border-[rgba(255,255,255,.1)] rounded-full absolute xs:top-[135px] xs:left-[160px] sm:top-[120px] sm:left-[160px] bg-background p-3 hover:bg-white hover:text-black"
+                    >
+                        <FaLinkedin size={25} />
+                    </Link>
+                    <Link
+                        href="/"
+                        className="border border-[rgba(255,255,255,.1)] rounded-full absolute xs:top-[230px] xs:left-[250px] sm:top-[220px] sm:left-[270px] bg-background p-3 hover:bg-white hover:text-black"
+                    >
+                        <FaGithub size={25} />
+                    </Link>
+                    <Link
+                        href="/"
+                        className="border border-[rgba(255,255,255,.1)] rounded-full absolute xs:top-[350px] xs:left-[270px] sm:top-[360px] sm:left-[290px] bg-background p-3 hover:bg-white hover:text-black"
+                    >
+                        <FaFacebookF size={25} />
+                    </Link>
+                    <ConcentricCircles
+                        className="left-[90px] -z-10 top-[350px]"
+                        variant="advanced"
+                    />
                 </div>
             </div>
         </div>
@@ -37,9 +72,9 @@ function Section2() {
 function Intro() {
     return (
         <div className="overflow-hidden">
-            <Wrapper className="flex min-w-max mt-[80px]">
-                <Section1 />
-                <Section2 />
+            <Wrapper className="flex xs:flex-col-reverse xs:mt-2 lg:flex-row lg:min-w-max lg:mt-[80px]">
+                <Section1 className="" />
+                <Section2 className="" />
             </Wrapper>
             <div className="relative">
                 <ConcentricCircles />
--- src/components/ui/ConcentricCircles.tsx
+++ src/components/ui/ConcentricCircles.tsx
@@ -1,28 +1,58 @@
 import { cn } from "@lib/utils";
 import React from "react";
 
-function Circle({ className }: { className?: string }) {
+function Circle({
+    className,
+    variant,
+}: {
+    className?: string;
+    variant?: "default" | "advanced";
+}) {
     return (
         <div
             className={cn(
                 `absolute top-[50%] left-[50%] border border-[rgba(255,255,255,.1)] rounded-full transform -translate-x-[50%] -translate-y-[50%]`,
-                className
+                className,
+                variant === "advanced" ? "border-secondary-foreground" : ""
             )}
-        ></div>
+        >
+            {/* {variant === "advanced" && (
+                <div className="relative hover:bg-white">
+
+                </div>
+            )} */}
+        </div>
     );
 }
 
-function ConcentricCircles({ className }: { className?: string }) {
-    return (
-        <div className={cn("absolute top-0", className)}>
-            {/* Smallest Circle */}
-            <Circle className="h-[340px] w-[340px]" />
-            {/* Medium Circle */}
-            <Circle className="h-[460px] w-[460px]" />
-            {/* Largest Circle */}
-            <Circle className="h-[600px] w-[600px]" />
-        </div>
-    );
+type PropType = { className?: string; variant?: "default" | "advanced" };
+function ConcentricCircles({ className, variant = "default" }: PropType) {
+    if (variant === "default") {
+        return (
+            <div className={cn("xs:hidden md:block absolute top-0", className)}>
+                {/* Smallest Circle */}
+                <Circle className="xs:h-[300px] xs:w-[300px] sm:h-[340px] sm:w-[340px]" />
+                {/* Medium Circle */}
+                <Circle className="xs:h-[420px] xs:w-[420px] sm:h-[460px] sm:w-[460px]" />
+                {/* Largest Circle */}
+                <Circle className="xs:h-[560px] xs:w-[560px] sm:h-[600px] sm:w-[600px]" />
+            </div>
+        );
+    } else if (variant === "advanced") {
+        return (
+            <div className={cn("absolute top-0", className)}>
+                {/* Smallest Circle */}
+                <Circle className="xs:h-[300px] xs:w-[300px] sm:h-[340px] sm:w-[340px]" />
+                {/* Medium Circle */}
+                <Circle
+                    className="xs:h-[420px] xs:w-[420px] sm:h-[460px] sm:w-[460px]"
+                    variant="advanced"
+                />
+                {/* Largest Circle */}
+                <Circle className="xs:h-[560px] xs:w-[560px] sm:h-[600px] sm:w-[600px]" />
+            </div>
+        );
+    }
 }
 
 export default ConcentricCircles;
