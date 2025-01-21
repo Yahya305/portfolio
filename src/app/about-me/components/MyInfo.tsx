aeec062e41ff2b7a54f9d42334e9ffb42f887b35
Yahya305 <saimyahya47@gmail.com>
2025-01-21 19:10:00 +0500

    add(ui): about me page

--- src/app/about-me/components/MyInfo.tsx
+++ src/app/about-me/components/MyInfo.tsx
@@ -1,3 +1,4 @@
+import GetResumeButton from "@components/GetResumeButton";
 import Wrapper from "@components/Wrapper";
 import Image from "next/image";
 import React from "react";
@@ -7,6 +8,7 @@ export default function MyInfo() {
         <Wrapper className="bg-secondary py-20 flex flex-col md:flex-row gap-10">
             <div className="relative w-[270px] h-[400px] mx-auto md:mx-0 md:w-[270px] md:h-[400px]">
                 <Image fill src={"/My_Full_Pic.jpg"} alt="Yahya Salman Pic" />
+                <GetResumeButton className="absolute hidden md:flex top-[380px] left-10" />
             </div>
             <div className="flex flex-col gap-5 flex-1">
                 <p>
@@ -36,6 +38,10 @@ export default function MyInfo() {
                     chess enthusiast—my favorite opening is the classic 1. e4.
                     Let's connect and create something remarkable together!
                 </p>
+                <div>
+
+                <GetResumeButton className="md:hidden mx-auto" />
+                </div>
             </div>
         </Wrapper>
     );
--- src/components/GetResumeButton.tsx
+++ src/components/GetResumeButton.tsx
@@ -1,9 +1,15 @@
+import { cn } from "@lib/utils";
 import React from "react";
 import { FiDownload } from "react-icons/fi";
 
-export default function GetResumeButton() {
+export default function GetResumeButton({ className }: { className?: string }) {
     return (
-        <button className="flex items-center gap-2 relative p-5 md:p-8 border rounded-[50px] bg-background hover:bg-border my-10 z-10">
+        <button
+            className={cn(
+                "flex items-center gap-2 relative p-5 md:p-8 border rounded-[50px] bg-background hover:bg-border my-10 z-10",
+                className
+            )}
+        >
             Get Resume <FiDownload size={20} />
         </button>
     );
